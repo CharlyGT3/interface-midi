ebf38190d0d0959f493ae49690c3f6b080d0b421
CharlyGT3 <Charlygt@live.com.ar>
2022-06-22 23:10:29 -0300

    Interface-Midi

--- midi.js
+++ midi.js
@@ -4,12 +4,38 @@ var switches = document.querySelector(".switches");
 var Add = document.querySelector(".Add");
 var Remove = document.querySelector(".Remove");
 
+function addLocalStorage () {
+    let saveNumSwitches = switches.childNodes.length;
+        let saveSwitches = switches.childNodes[saveNumSwitches - 1].outerHTML;
+        localStorage.setItem("switches" + (saveNumSwitches - 1), saveSwitches);
+}
+
+function removeLocalStorage () {
+    let saveNumSwitches = switches.childNodes.length;
+        localStorage.removeItem("switches" + saveNumSwitches);
+        }
+
+function insertLocalStorage () {
+    let localStorageLength = localStorage.length;
+    for (var i = 0; i < localStorageLength; i++) {
+        let lSSW = localStorage.getItem("switches" + (i + 1));
+        switches.insertAdjacentHTML("beforeend", lSSW);
+    }
+}
+
+insertLocalStorage();
+
 Add.addEventListener("click", function() {
     switches.insertAdjacentHTML("beforeend", options);
+    addLocalStorage();
 }
 );
 Remove.addEventListener("click", function() {
+    let saveNumSwitches = switches.childNodes.length;
+    if (saveNumSwitches > 0) {
     switches.removeChild(switches.lastChild);
+    removeLocalStorage();
+    }
 }
 );
 
